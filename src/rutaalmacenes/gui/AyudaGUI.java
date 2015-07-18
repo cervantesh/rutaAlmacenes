e15777a7a851611495b4d4ec4d9151c10d92e71b
cervantesh <cervantes.hernandezmoreira@gmail.com>
2015-07-18 05:54:28 -0400

    El diseño inicial de la interfaz grafica del proyecto. Falta validar todo. Faltan diseñar el FormCaminos, Form Creditos y el FormCalculoRutaOptima.

---
+++ src/rutaalmacenes/gui/AyudaGUI.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rutaalmacenes.gui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//import javax.swing.DefaultComboBoxModel;
+
+/**
+ *
+ * @author Seelee
+ */
+public class AyudaGUI {
+    
+    private void llenarComboBox(javax.swing.JComboBox jc)//, Residencial res)
+    {
+        /*
+        javax.swing.DefaultComboBoxModel model = new javax.swing.DefaultComboBoxModel();
+      //  Edificio  aux = null;
+        
+        if (res!=null) 
+            for (int i = 0; i < res.getCantidadEdificios(); i++) {
+
+                
+            try {
+                aux = res.getResidencial().getLista().obtener(i);
+            } catch (Exception ex) {
+                Logger.getLogger(FormAgregarCamino.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+
+                model.addElement(aux);
+
+            }
+        jc.setModel(model);
+        */
+       
+    }
+                
+    
+}
